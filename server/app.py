4c4292b8605e30960eaff53f7babe67ed2c0e91f
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-05-26 15:00:06 +0300

    Add uploading files

--- server/app.py
+++ server/app.py
@@ -1,20 +1,70 @@
 import time
 import os
-from flask import Flask, request, Response
+from flask import Flask, request, Response, abort, flash, redirect, url_for
+from flask import send_from_directory, render_template
+from werkzeug.utils import secure_filename
 
+
+UPLOAD_FOLDER = './data'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+#
+# @app.route('/')
+# def static_file():
+#     message = "Hello, World"
+#     return render_template('index.html', message=message)
 
-@app.route('/my', methods=['GET'])
-def execute_script():
-    start_time = time.time()
-    print('aaaaa')
-    response = Response(
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+# @app.route('/api/users', methods = ['POST'])
+# def new_user():
+#     username = request.json.get('username')
+#     password = request.json.get('password')
+#     if username is None or password is None:
+#         abort(400) # missing arguments
+#     if User.query.filter_by(username = username).first() is not None:
+#         abort(400) # existing user
+#     user = User(username = username)
+#     user.hash_password(password)
+#     db.session.add(user)
+#     db.session.commit()
+#     return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
 
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            print('aaaaaa')
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
-    app.run(debug=True, port='5000', host='0.0.0.0', use_reloader=False)
+    app.run(debug=True, port='5000', host='0.0.0.0')
