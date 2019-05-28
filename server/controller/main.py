4b27354bd95f95d54e57921419f9d4da562cca3d
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-05-28 14:02:05 +0300

    Add summary to db

--- server/controller/main.py
+++ server/controller/main.py
@@ -8,6 +8,10 @@ from flask import current_app as app
 from werkzeug.utils import secure_filename
 
 from server.controller.nn_tools import get_predictions
+from server.model.models import Summary
+from server import db
+
+from flask import g
 
 main = Blueprint('main', __name__)
 
@@ -43,13 +47,13 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             summary = get_predictions(os.path.join(app.config['UPLOAD_FOLDER'],filename))
             return redirect(url_for('main.upload_summary',
-                                    summary=summary))
+                                    summary=summary, filename=filename))
     return render_template('index.html')
 
 
-@main.route('/upload_summary/<string:summary>')
+@main.route('/upload_summary/<string:summary>/<string:filename>')
 @login_required
-def upload_summary(summary):
+def upload_summary(summary, filename):
     j = json.loads(summary)
     emotions=['neutral', 'happy', 'sad', 'hate', 'anger']
     neutral = j['counts']['0']
@@ -57,4 +61,9 @@ def upload_summary(summary):
     sad = j['counts']['2']
     hate = j['counts']['3']
     anger = j['counts']['4']
+
+    # TODO user_id
+    new_summary = Summary(user_id=3,name=filename, hate_cnt=hate, neutal_cnt=neutral, happy_cnt=happy, sad_cnt=sad, anger_cnt=anger)
+    db.session.add(new_summary)
+    db.session.commit()
     return render_template('summary.html', emotions=emotions, happy=happy, neutral=neutral, sad=sad,hate=hate, anger=anger, pr=j['predictions'])
