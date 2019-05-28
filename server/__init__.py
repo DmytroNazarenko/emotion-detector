173d697917648158f623c95aedf56602462d7631
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-05-28 13:03:19 +0300

    Add migrations

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,8 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -15,6 +16,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
--- server/model/models.py
+++ server/model/models.py
@@ -1,10 +1,22 @@
 # models.py
+from datetime import datetime
 
 from flask_login import UserMixin
 from server import db
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.name)
